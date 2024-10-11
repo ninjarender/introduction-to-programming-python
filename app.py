7c260e909519f074d2a8d2b45b07967a0a667650
Vadym Kruchyna <kruchina.vadim@gmail.com>
2024-10-12 00:43:24 +0200

    input nickname

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 print("I'm learning Git merge conflicts")
+
 name = input('Please enter your name: ')
 last_name = input('Please enter your last name: ')
-print('Hi, {} {}!'.format(name, last_name))
+nick = input('Please enter your nickname: ')
+
+print('Hi, {} {} a.k.a {}!'.format(name, last_name, nick))
